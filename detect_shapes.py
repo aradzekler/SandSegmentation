634c3634633d9faed3c1fb267b38758539284454
aradzekler <mcspancy@gmail.com>
2021-01-06 19:30:29 +0200

    minor changes

--- detect_shapes.py
+++ detect_shapes.py
@@ -3,7 +3,6 @@ import argparse
 import imutils
 import cv2
 
-
 '''
 !!IMPORTANT!!
 
@@ -11,6 +10,11 @@ In order to activate this thing on an image, input the terminal with
 python detect_shapes.py --image Mill_10666.tif and press SPACE (or any key)
 in order to advance the script
 '''
+
+light_orange = (0, 0, 20)
+dark_orange = (200, 255, 255)
+
+# CREATING A SCATTER PLOT
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -21,12 +25,17 @@ args = vars(ap.parse_args())
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=300)
-ratio = image.shape[0] / float(resized.shape[0])
+
+hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)  # converting the color of the image
+mask = cv2.inRange(hsv_image, light_orange, dark_orange)
+result = cv2.bitwise_and(image, image, mask=mask)
+
+result = imutils.resize(result, width=1000)
+ratio = result.shape[0] / float(result.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
@@ -42,7 +51,7 @@ for c in contours:
 	# compute the center of the contour, then detect the name of the
 	# shape using only the contour
 	M = cv2.moments(c)
-	cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
+	cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)  # added 1e-7 to the calculation so to not multiply by 0
 	cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
 	shape = sd.detect(c)
 
@@ -51,10 +60,10 @@ for c in contours:
 	c = c.astype("float")
 	c *= ratio
 	c = c.astype("int")
-	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+	cv2.drawContours(result, [c], -1, (0, 255, 0), 2)
+	cv2.putText(result, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
 	            0.5, (255, 255, 255), 2)
 
 	# show the output image
-	cv2.imshow("Image", image)
+	cv2.imshow("Image", result)
 	cv2.waitKey(0)
